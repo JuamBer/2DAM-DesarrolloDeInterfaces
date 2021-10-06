5c549a38a68eb15e0a325f4677b427a62c414669
Juan Sáez García <contacto@juamber.com>
2021-10-06 13:35:28 +0200

    mcorm

--- 011.Monocrom/js/app.js
+++ 011.Monocrom/js/app.js
@@ -4,20 +4,82 @@ let pantalla = document.getElementById("screen");
 const num = 16;
 
 btn.addEventListener('click', () => {
-        var table = new Array(num);
-        for (var i = 1; i <= num; i++) {
-            table[i] = new Array(num);
-        }
-        for(var i = 1; i <= num;i++){
-            for (var j = 1; j <= num; j++) {
-                table[i][j]=0;
+        var valores = input.value.split(" ");
+        valores.forEach(element => {
+            element = parseInt(element);
+            console.log(element);
+        });
+        if(valores.length>4){
+            pantalla.innerHTML = "Demasiados parámetros";
+        }else{
+            var table = new Array(num);
+            for (var i = 1; i <= num; i++) {
+                table[i] = new Array(num);
             }
+            for (var i = 1; i <= num; i++) {
+                for (var j = 1; j <= num; j++) {
+                    table[i][j] = 0;
+                }
+            }
+                let total = table.length - 1;
+                let mitad = total / 2;
+            filltable(table, valores,total,mitad);
+            
+            pantalla.innerHTML = getScreen(table);
         }
-        console.log(getScreen(table));
-        pantalla.innerHTML = getScreen(table);
+        
+        
+
+        
 
 });
+function filltable(table, valores,total,mitad) {
+    console.log("FILLTABLE: valores.length="+valores.length+"total="+total+"mitad="+mitad);
 
+    if (valores.length == 1) {
+        switch(valores[1]){
+            case 1: forfilltable(table,1,1,mitad,mitad); break;
+            case 2: forfilltable(table, 1, mitad+1, mitad, total);
+            break;
+            case 3: forfilltable(table, mitad + 1, mitad + 1, total, total);
+            break;
+            case 4: forfilltable(table, mitad +1, 1, total, mitad );
+            break;
+            default: break;
+        }
+    }else{
+        console.log("valores[1]"+valores[1]);
+        switch (valores[1]) {
+            case 1:
+                    total = (table.length - 1)/2;
+                    mitad = (total / 2)/2;
+                    valores.splice(1, 1);
+                    filltable(table,valores,total,mitad);
+                break;
+            case 2:
+                forfilltable(table, 1, mitad + 1, mitad, total);
+                break;
+            case 3:
+                forfilltable(table, mitad + 1, mitad + 1, total, total);
+                break;
+            case 4:
+                forfilltable(table, mitad + 1, 1, total, mitad);
+                break;
+            default:
+                break;
+        }
+    }
+    return table;
+}
+function forfilltable(table, starti, startj, stopi,stopj) {
+   for(var i = starti; i <=  stopi; i++){
+       for (var j = startj; j <= stopj; j++) {
+            table[i][j]="X";
+       }
+   }
+   console.log(table);
+   return table;
+}
 function getScreen(table) {
     var texto = "";
     for (i = 1; i <= num; i++) {
