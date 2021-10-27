e8b62ea50689718f9e3a2fea475a9b0dd2d96d5c
Juan Sáez García <contacto@juamber.com>
2021-10-27 16:01:05 +0200

    puzzle

--- 015.Puzzle/js/app.js
+++ 015.Puzzle/js/app.js
@@ -1,39 +1,44 @@
 const {
     dialog
-
 } = require('@electron/remote');
 
-let img00 = document.getElementById("img00");
-let img01 = document.getElementById("img01");
-let img02 = document.getElementById("img02");
-let img03 = document.getElementById("img03");
-let img10 = document.getElementById("img10");
-let img11 = document.getElementById("img11");
-let img12 = document.getElementById("img12");
-let img13 = document.getElementById("img13");
-let img20 = document.getElementById("img20");
-let img21 = document.getElementById("img21");
-let img22 = document.getElementById("img22");
-let img23 = document.getElementById("img23");
-let img30 = document.getElementById("img30");
-let img31 = document.getElementById("img31");
-let img32 = document.getElementById("img32");
-let img33 = document.getElementById("img33");
 
 const LON = 4;
-var images = new Array(LON);
-
-for (let i = 0; i < LON; i++) {
-    images[i] = new Array(LON);
-}
-
+var images;
+var result;
 var xwhite;
 var ywhite;
 
+fillImages(LON);
+fillResult(LON);
 createPuzzle(LON);
 searchWhite(LON);
-createListeners(LON);
-removeListeners(LON);
+createListeners(xwhite, ywhite, LON);
+
+console.log(images);
+console.log(result);
+
+function fillImages(lon){
+    images = new Array(LON);
+
+    for (let i = 0; i < LON; i++) {
+        images[i] = new Array(LON);
+    }
+}
+function fillResult(lon) {
+    result = new Array(LON);
+
+    for (let i = 0; i < LON; i++) {
+        result[i] = new Array(LON);
+    }
+
+    for (let x = 0; x < lon; x++) {
+        for (let y = 0; y < lon; y++) {
+            result[x][y] = "../img/" + x.toString() + y.toString() + ".jpg";
+        }
+    }
+    result[0][0] = "../img/white.jpg";
+}
 
 function createPuzzle(lon) {
     console.log("\n-------createPuzzle-------");
@@ -42,6 +47,7 @@ function createPuzzle(lon) {
             images[x][y] = "../img/" + x.toString() + y.toString() + ".jpg";
         }
     }
+
     images[0][0] = "../img/white.jpg";
 
     for (let x = 0; x < lon; x++) {
@@ -49,20 +55,19 @@ function createPuzzle(lon) {
             let aux = images[x][y];
             let ran1 = Math.round(Math.random() * 3);
             let ran2 = Math.round(Math.random() * 3);
-
+    
             images[x][y] = images[ran1][ran2];
             images[ran1][ran2] = aux;
         }
     }
-    seeImgages(lon);
-
+    seeImages(lon);
 }
 
-function seeImgages(lon) {
+function seeImages(lon) {
     console.log("\n-------seeImages-------");
     for (let x = 0; x < lon; x++) {
         for (let y = 0; y < lon; y++) {
-            let image = images[x][y].toString();
+            let image = images[x][y];
             let src = "img" + x + "" + y;
             document.getElementById(src).src = image;
         }
@@ -82,166 +87,168 @@ function searchWhite(lon) {
     }
 }
 
-function changeImgUp() {
-    console.log("\n-------changeImg Up-------");
-    let variablex = xwhite - 1;
-    let variabley = ywhite;
-    document.getElementById("img" + variablex.toString() + "" + variabley.toString()).style.background = '#2f2f2f';
-    let aux = images[variablex][variabley];
-    images[variablex][variabley] = images[xwhite][ywhite];
-    images[xwhite][ywhite] = aux;
-
-    xwhite = variablex;
-    ywhite = variabley;
-    console.log("X: " + xwhite + " Y: " + ywhite);
-    removeListeners(LON);
-    seeImgages(LON);
-}
-
-function createListeners(lon) {
+function createListeners(x, y, lon) {
     console.log("\n-------createListeners-------");
 
-    if (!((xwhite - 1) < 0)) {
-        let variablexup = xwhite - 1;
-        let variableyup = ywhite;
+    if (!((x - 1) < 0)) {
+        let variablexup = x - 1;
+        let variableyup = y;
 
         let src = "img" + variablexup + "" + variableyup;
         console.log("Creating Listener Up Imagen: " + src);
         document.getElementById(src).style.background = '#07DE5D';
-        document.getElementById(src).addEventListener('click', changeImgUp, {
-            once: true
-        });
+        document.getElementById(src).addEventListener('click', changeImgUp);
     }
 
-    if (!((xwhite + 1) >= lon - 1)) {
-        let variablexdown = xwhite + 1;
-        let variableydown = ywhite;
+    if (!((x + 1) >= lon)) {
+        let variablexdown = x + 1;
+        let variableydown = y;
 
         let src = "img" + variablexdown + "" + variableydown;
         console.log("Creating Listener Down Imagen: " + src);
         document.getElementById(src).style.background = '#07DE5D';
-        document.getElementById(src).addEventListener('click', changeImgDown, {
-            once: true
-        });
+        document.getElementById(src).addEventListener('click', changeImgDown);
     }
-    if (!((ywhite - 1) < 0)) {
-        let variablexleft = xwhite;
-        let variableyleft = ywhite - 1;
+    if (!((y - 1) < 0)) {
+        let variablexleft = x;
+        let variableyleft = y - 1;
 
         let src = "img" + variablexleft + "" + variableyleft;
         console.log("Creating Listener Left Imagen: " + src);
         document.getElementById(src).style.background = '#07DE5D';
-        document.getElementById(src).addEventListener('click', changeImgLeft, {
-            once: true
-        });
+        document.getElementById(src).addEventListener('click', changeImgLeft);
     }
-    if (!((ywhite + 1) >= lon - 1)) {
-        let variablexrigth = xwhite;
-        let variableyrigth = ywhite + 1;
+    if (!((y + 1) >= lon)) {
+        let variablexrigth = x;
+        let variableyrigth = y + 1;
 
         let src = "img" + variablexrigth + "" + variableyrigth;
         console.log("Creating Listener Rigth Imagen: " + src);
         document.getElementById(src).style.background = '#07DE5D';
-        document.getElementById(src).addEventListener('click', changeImgRight, {
-            once: true
-        });
+        document.getElementById(src).addEventListener('click', changeImgRight);
     }
 
 }
+function checkWin(lon){
+    console.log("-----CheckWin-----");
+    let key = true;
+    for (let x = 0; x < lon; x++) {
+        for (let y = 0; y < lon; y++) {
+            if (!(images[x][y] == result[x][y])) {
+                key = false;
+                break;
+            }
+        }
+    }
+    console.log(key);
+    if(key){
+        images[0][0] = "../img/00.jpg";
+        document.getElementById("img00").src = images[0][0];
+        dialog.showMessageBox({
+            message: 'Win !',
+            type: 'info',
+            buttons: ['Okay'],
+            title: 'Puzzle'
+        });
+    }
+}
+function changeImg(variablex, variabley, lon) {
+    let aux = images[variablex][variabley];
+    images[variablex][variabley] = images[xwhite][ywhite];
+    images[xwhite][ywhite] = aux;
 
+    removeListeners(xwhite, ywhite, variablex, variabley, lon);
+    seeImages(lon);
 
+    xwhite = variablex;
+    ywhite = variabley;
+
+    checkWin(lon);
+    console.log("X: " + xwhite + " Y: " + ywhite);
+
+}
+
+function changeImgUp() {
+    console.log("\n-------changeImg Up-------");
+    let variablex = xwhite - 1;
+    let variabley = ywhite;
+
+    changeImg(variablex, variabley, LON);
+}
 
 function changeImgDown() {
     console.log("\n-------changeImg Down-------");
     let variablex = xwhite + 1;
     let variabley = ywhite;
-    document.getElementById("img" + variablex.toString() + "" + variabley.toString()).style.background = '#2f2f2f';
-    let aux = images[variablex][variabley];
-    images[variablex][variabley] = images[xwhite][ywhite];
-    images[xwhite][ywhite] = aux;
 
-    xwhite = variablex;
-    ywhite = variabley;
-    console.log("X: " + xwhite + " Y: " + ywhite);
-    removeListeners(LON);
-    seeImgages(LON);
+    changeImg(variablex, variabley, LON);
 }
 
 function changeImgLeft() {
     console.log("\n-------changeImg Left-------");
     let variablex = xwhite;
     let variabley = ywhite - 1;
-    document.getElementById("img" + variablex.toString() + "" + variabley.toString()).style.background = '#2f2f2f';
-    let aux = images[variablex][variabley];
-    images[variablex][variabley] = images[xwhite][ywhite];
-    images[xwhite][ywhite] = aux;
 
-    xwhite = variablex;
-    ywhite = variabley;
-    console.log("X: " + xwhite + " Y: " + ywhite);
-    removeListeners(LON);
-    seeImgages(LON);
-
+    changeImg(variablex, variabley, LON);
 }
 
 function changeImgRight() {
     console.log("\n-------changeImg Right-------");
     let variablex = xwhite;
     let variabley = ywhite + 1;
-    document.getElementById("img" + variablex.toString() + "" + variabley.toString()).style.background = '#2f2f2f';
-    let aux = images[variablex][variabley];
-    images[variablex][variabley] = images[xwhite][ywhite];
-    images[xwhite][ywhite] = aux;
 
-    xwhite = variablex;
-    ywhite = variabley;
-    console.log("X: " + xwhite + " Y: " + ywhite);
-    removeListeners(LON);
-    seeImgages(LON);
-
+    changeImg(variablex, variabley, LON);
 }
 
-function removeListeners(lon) {
+function removeListeners(x, y, createx, createy, lon) {
     console.log("\n-------removeListeners-------");
-    let variablex = null;
-    let variabley = null;
+    let variablex;
+    let variabley;
     let src = null;
 
-    if (!((xwhite - 1) < 0)) {
-        variablex = xwhite - 1;
-        variabley = ywhite;
+    if (!((x - 1) < 0)) {
+        variablex = x - 1;
+        variabley = y;
         src = "img" + variablex + "" + variabley;
 
-        document.getElementById(src).style.background = '#FFFFFF';
+
         console.log('Removing Up Listener: ' + src);
+        document.getElementById(src).style.background = '#2F2F2F';
+        document.getElementById(src).removeEventListener('click', changeImgUp);
     }
 
-    if (!((xwhite + 1) >= lon - 1)) {
-        variablex = xwhite + 1;
-        variabley = ywhite;
+    if (!((x + 1) >= lon)) {
+        variablex = x + 1;
+        variabley = y;
         src = "img" + variablex + "" + variabley;
 
-        document.getElementById(src).style.background = '#FFFFFF';
+
         console.log('Removing Down Listener: ' + src);
+        document.getElementById(src).style.background = '#2F2F2F';
+        document.getElementById(src).removeEventListener('click', changeImgDown);
     }
 
-    if (!((ywhite - 1) < 0)) {
-        variablex = xwhite;
-        variabley = ywhite - 1;
+    if (!((y - 1) < 0)) {
+        variablex = x;
+        variabley = y - 1;
         src = "img" + variablex + "" + variabley;
+
         console.log('Removing Left Listener: ' + src);
-        document.getElementById(src).style.background = '#FFFFFF';
+        document.getElementById(src).style.background = '#2F2F2F';
+        document.getElementById(src).removeEventListener('click', changeImgLeft);
+
     }
 
-    if (!((ywhite + 1) >= lon - 1)) {
-        variablex = xwhite;
-        variabley = ywhite + 1;
+    if (!((y + 1) >= lon)) {
+        variablex = x;
+        variabley = y + 1;
         src = "img" + variablex + "" + variabley;
-        console.log('Removing Rigth Listener: ' + src);
-        document.getElementById(src).style.background = '#FFFFFF';
-    }
 
-    createListeners(LON);
+        console.log('Removing Rigth Listener: ' + src);
+        document.getElementById(src).style.background = '#2F2F2F';
+        document.getElementById(src).removeEventListener('click', changeImgRight);
+    }
+    createListeners(createx, createy, lon);
 }
 
 function a() {
