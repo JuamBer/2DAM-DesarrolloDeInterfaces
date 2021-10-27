74b65fd75d5f7edfe6fc54af883c20e2403ce7e7
Juan Sáez García <contacto@juamber.com>
2021-10-27 11:10:40 +0200

    puzzle

--- 015.Puzzle/js/app.js
+++ 015.Puzzle/js/app.js
@@ -30,31 +30,13 @@ for (let i = 0; i < LON; i++) {
 var xwhite;
 var ywhite;
 
-createPuzzle(images, LON);
-searchWhite(images, LON);
-createListeners(images, LON, xwhite, ywhite);
+createPuzzle(LON);
+searchWhite(LON);
+createListeners(LON);
+removeListeners(LON);
 
-/*
-img0.addEventListener('click',changeImg(images,0,0));
-img1.addEventListener('click', changeImg(images, x, y));
-img2.addEventListener('click', changeImg(images, x, y));
-img3.addEventListener('click', changeImg(images, x, y));
-img4.addEventListener('click', changeImg(images, x, y));
-img5.addEventListener('click', changeImg(images, x, y));
-img6.addEventListener('click', changeImg(images, x, y));
-img7.addEventListener('click', changeImg(images, x, y));
-img8.addEventListener('click', changeImg(images, x, y));
-img9.addEventListener('click', changeImg(images, x, y));
-img10.addEventListener('click', changeImg(images, x, y));
-img11.addEventListener('click', changeImg(images, x, y));
-img12.addEventListener('click', changeImg(images, x, y));
-img13.addEventListener('click', changeImg(images, x, y));
-img14.addEventListener('click', changeImg(images, x, y));
-img15.addEventListener('click', changeImg(images, x, y));*/
-
-
-
-function createPuzzle(images, lon) {
+function createPuzzle(lon) {
+    console.log("\n-------createPuzzle-------");
     for (let x = 0; x < lon; x++) {
         for (let y = 0; y < lon; y++) {
             images[x][y] = "../img/" + x.toString() + y.toString() + ".jpg";
@@ -72,10 +54,12 @@ function createPuzzle(images, lon) {
             images[ran1][ran2] = aux;
         }
     }
-    seeImgages(images,lon);
-    
+    seeImgages(lon);
+
 }
-function seeImgages(images, lon) {
+
+function seeImgages(lon) {
+    console.log("\n-------seeImages-------");
     for (let x = 0; x < lon; x++) {
         for (let y = 0; y < lon; y++) {
             let image = images[x][y].toString();
@@ -85,62 +69,181 @@ function seeImgages(images, lon) {
     }
 }
 
-function searchWhite(images, lon) {
+function searchWhite(lon) {
+    console.log("\n-------searchWhite-------");
     for (let x = 0; x < lon; x++) {
         for (let y = 0; y < lon; y++) {
             if (images[x][y] == "../img/white.jpg") {
                 xwhite = x;
                 ywhite = y;
-                break;
+                console.log("X: " + xwhite + " Y: " + xwhite);
             }
         }
     }
 }
 
+function changeImgUp() {
+    console.log("\n-------changeImg Up-------");
+    let variablex = xwhite - 1;
+    let variabley = ywhite;
+    document.getElementById("img" + variablex.toString() + "" + variabley.toString()).style.background = '#2f2f2f';
+    let aux = images[variablex][variabley];
+    images[variablex][variabley] = images[xwhite][ywhite];
+    images[xwhite][ywhite] = aux;
 
-function createListeners(images, lon, x, y) {
-    let variablex = x;
-    let variabley = y;
+    xwhite = variablex;
+    ywhite = variabley;
+    console.log("X: " + xwhite + " Y: " + ywhite);
+    removeListeners(LON);
+    seeImgages(LON);
+}
 
-    if (!((x - 1) < 0)) {
-        variablex = x - 1;
-        let src = "img" + variablex + "" + variabley;
-        document.getElementById(src).addEventListener('click', () => {
-            changeImg(images, variablex, variabley, x, y)
+function createListeners(lon) {
+    console.log("\n-------createListeners-------");
+
+    if (!((xwhite - 1) < 0)) {
+        let variablexup = xwhite - 1;
+        let variableyup = ywhite;
+
+        let src = "img" + variablexup + "" + variableyup;
+        console.log("Creating Listener Up Imagen: " + src);
+        document.getElementById(src).style.background = '#07DE5D';
+        document.getElementById(src).addEventListener('click', changeImgUp, {
+            once: true
         });
-        console.log('Up AddEventListener');
     }
-    if (!((x + 1) >= lon)) {
-        variablex = x + 1;
-        let src = "img" + variablex + "" + variabley;
-        document.getElementById(src).addEventListener('click', () => {
-            changeImg(images, variablex, variabley, x, y)
+
+    if (!((xwhite + 1) >= lon - 1)) {
+        let variablexdown = xwhite + 1;
+        let variableydown = ywhite;
+
+        let src = "img" + variablexdown + "" + variableydown;
+        console.log("Creating Listener Down Imagen: " + src);
+        document.getElementById(src).style.background = '#07DE5D';
+        document.getElementById(src).addEventListener('click', changeImgDown, {
+            once: true
         });
-        console.log('Down AddEventListener');
     }
-    if (!((y - 1) < 0)) {
-        variabley = y - 1;
-        let src = "img" + variablex + "" + variabley;
-        document.getElementById(src).addEventListener('click', () => {
-            changeImg(images, variablex, variabley, x, y)
+    if (!((ywhite - 1) < 0)) {
+        let variablexleft = xwhite;
+        let variableyleft = ywhite - 1;
+
+        let src = "img" + variablexleft + "" + variableyleft;
+        console.log("Creating Listener Left Imagen: " + src);
+        document.getElementById(src).style.background = '#07DE5D';
+        document.getElementById(src).addEventListener('click', changeImgLeft, {
+            once: true
         });
-        console.log('Left AddEventListener');
     }
-    if (!((y + 1) >= lon)) {
-        variabley = y + 1;
-        let src = "img" + variablex + "" + variabley;
-        document.getElementById(src).addEventListener('click', () => {
-            changeImg(images, variablex, variabley, x, y)
+    if (!((ywhite + 1) >= lon - 1)) {
+        let variablexrigth = xwhite;
+        let variableyrigth = ywhite + 1;
+
+        let src = "img" + variablexrigth + "" + variableyrigth;
+        console.log("Creating Listener Rigth Imagen: " + src);
+        document.getElementById(src).style.background = '#07DE5D';
+        document.getElementById(src).addEventListener('click', changeImgRight, {
+            once: true
         });
-        console.log('Rigth AddEventListener');
     }
+
 }
 
 
-function changeImg(images, variablex, variabley, x, y) {
-    console.log('Clicked');
+
+function changeImgDown() {
+    console.log("\n-------changeImg Down-------");
+    let variablex = xwhite + 1;
+    let variabley = ywhite;
+    document.getElementById("img" + variablex.toString() + "" + variabley.toString()).style.background = '#2f2f2f';
     let aux = images[variablex][variabley];
-    images[variablex][variabley] = images[x][y];
-    images[x][y] = aux;
-    seeImgages(images, LON);
+    images[variablex][variabley] = images[xwhite][ywhite];
+    images[xwhite][ywhite] = aux;
+
+    xwhite = variablex;
+    ywhite = variabley;
+    console.log("X: " + xwhite + " Y: " + ywhite);
+    removeListeners(LON);
+    seeImgages(LON);
+}
+
+function changeImgLeft() {
+    console.log("\n-------changeImg Left-------");
+    let variablex = xwhite;
+    let variabley = ywhite - 1;
+    document.getElementById("img" + variablex.toString() + "" + variabley.toString()).style.background = '#2f2f2f';
+    let aux = images[variablex][variabley];
+    images[variablex][variabley] = images[xwhite][ywhite];
+    images[xwhite][ywhite] = aux;
+
+    xwhite = variablex;
+    ywhite = variabley;
+    console.log("X: " + xwhite + " Y: " + ywhite);
+    removeListeners(LON);
+    seeImgages(LON);
+
+}
+
+function changeImgRight() {
+    console.log("\n-------changeImg Right-------");
+    let variablex = xwhite;
+    let variabley = ywhite + 1;
+    document.getElementById("img" + variablex.toString() + "" + variabley.toString()).style.background = '#2f2f2f';
+    let aux = images[variablex][variabley];
+    images[variablex][variabley] = images[xwhite][ywhite];
+    images[xwhite][ywhite] = aux;
+
+    xwhite = variablex;
+    ywhite = variabley;
+    console.log("X: " + xwhite + " Y: " + ywhite);
+    removeListeners(LON);
+    seeImgages(LON);
+
+}
+
+function removeListeners(lon) {
+    console.log("\n-------removeListeners-------");
+    let variablex = null;
+    let variabley = null;
+    let src = null;
+
+    if (!((xwhite - 1) < 0)) {
+        variablex = xwhite - 1;
+        variabley = ywhite;
+        src = "img" + variablex + "" + variabley;
+
+        document.getElementById(src).style.background = '#FFFFFF';
+        console.log('Removing Up Listener: ' + src);
+    }
+
+    if (!((xwhite + 1) >= lon - 1)) {
+        variablex = xwhite + 1;
+        variabley = ywhite;
+        src = "img" + variablex + "" + variabley;
+
+        document.getElementById(src).style.background = '#FFFFFF';
+        console.log('Removing Down Listener: ' + src);
+    }
+
+    if (!((ywhite - 1) < 0)) {
+        variablex = xwhite;
+        variabley = ywhite - 1;
+        src = "img" + variablex + "" + variabley;
+        console.log('Removing Left Listener: ' + src);
+        document.getElementById(src).style.background = '#FFFFFF';
+    }
+
+    if (!((ywhite + 1) >= lon - 1)) {
+        variablex = xwhite;
+        variabley = ywhite + 1;
+        src = "img" + variablex + "" + variabley;
+        console.log('Removing Rigth Listener: ' + src);
+        document.getElementById(src).style.background = '#FFFFFF';
+    }
+
+    createListeners(LON);
+}
+
+function a() {
+
 }
