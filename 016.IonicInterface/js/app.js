e4b83d596a1b99e5b4d765c9b35d706ec294de0c
Juan Sáez García <contacto@juamber.com>
2021-10-29 11:27:30 +0200

    ionic

--- 016.IonicInterface/js/app.js
+++ 016.IonicInterface/js/app.js
@@ -1,30 +1,95 @@
-let btn = document.getElementById('btn');
-let input = document.getElementById('input');
+let list = document.getElementById('SongsList');
+let btnanterior = document.getElementById('btnanterior');
+let btnpausa = document.getElementById('btnpausa');
+let btnplay = document.getElementById('btnplay');
+let btnsiguiente = document.getElementById('btnsiguiente');
+let bar = document.getElementById('progresiveBar');
 
-function presentAlertConfirm(input) {
-    const alert = document.createElement('ion-alert');
-    alert.cssClass = 'my-custom-class';
-    alert.header = 'Confirm!';
-    alert.message = input;
-    alert.buttons = [{
-        text: 'Cancel',
-        role: 'cancel',
-        cssClass: 'secondary',
-        handler: (blah) => {
-            console.log('Confirm Cancel: blah');
-        }
-    }, {
-        text: 'Okay',
-        handler: () => {
-            console.log('Confirm Okay')
-        }
-    }];
+let key = true;
+const fs = require('fs');
 
-    document.body.appendChild(alert);
-    return alert.present();
+let file = fs.readFileSync('db/songs.json');
+let songs = new Array();
+songs = JSON.parse(file);
+let pos = 0;
+let cont;
+let contTotal;
+
+loadList();
+
+function loadList() {
+    let htmlel;
+    contTotal = songs[pos].douration;
+    for (let i = 0; i < songs.length; i++) {
+        if (i == pos) {
+            cont = songs[pos].douration;
+            
+            htmlel = "<ion-item class='activated'> <ion-label>" + songs[pos].name + "</ion-label> <ion-label>" + songs[pos].group + "</ion-label> <ion-label>" + cont + "</ion-label> <ion-label><ion-icon name='musical-note-outline'></ion-icon></ion-label></ion-item>";
+            list.innerHTML += htmlel;
+           
+
+            minusSong();
+        } else {
+            htmlel = "<ion-item> <ion-label>" + songs[i].name + "</ion-label> <ion-label>" + songs[i].group + "</ion-label> <ion-label>" + songs[i].douration + "</ion-label> <ion-label></ion-label></ion-item>";
+            list.innerHTML += htmlel
+        }
+    }
 }
 
+function reloadList() {
+    console.log("s")
+    list.innerHTML = "";
+    contTotal = songs[pos].douration;
+    for (let i = 0; i < songs.length; i++) {
+        if (i == pos) {
+            
+            let htmlel = "<ion-item class='activated'> <ion-label>" + songs[pos].name + "</ion-label> <ion-label>" + songs[pos].group + "</ion-label> <ion-label>" + cont + "</ion-label> <ion-label><ion-icon name='musical-note-outline'></ion-icon></ion-label></ion-item>";
+            list.innerHTML += htmlel;
 
-btn.addEventListener('click', () => {
-    presentAlertConfirm(input.value);
+        } else {
+            let htmlel = "<ion-item> <ion-label>" + songs[i].name + "</ion-label> <ion-label>" + songs[i].group + "</ion-label> <ion-label>" + songs[i].douration + "</ion-label> <ion-label></ion-label></ion-item>";
+            list.innerHTML += htmlel
+        }
+
+    }
+    bar.value = cont / contTotal;
+
+}
+
+btnanterior.addEventListener('click', () => {
+    pos--;
+    cont = songs[pos].douration;
+    reloadList();
 });
+btnpausa.addEventListener('click', () => {
+    pauseSong();
+});
+btnplay.addEventListener('click', () => {
+    playSong();
+});
+btnsiguiente.addEventListener('click', () => {
+    pos++;
+    cont = songs[pos].douration;
+    reloadList();
+});
+function pauseSong(){
+    key = false;
+}
+function playSong() {
+    key = true;
+    minusSong();
+}
+function minusSong(){
+    if(cont == 0){
+       pos++; 
+       cont = songs[pos].douration;
+       reloadList();
+    }else{
+        if (key) {
+            cont--;
+
+            setTimeout("minusSong()", 1000);
+            setTimeout("reloadList()", 1000);
+        }
+    }
+}
