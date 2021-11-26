1de5e1a3e7a4c897da7a7756556b4e509907ee04
Juan Sáez García <contacto@juamber.com>
2021-11-26 12:28:41 +0100

    mongo

--- 019.Examen1EvMongoDB/js/app.js
+++ 019.Examen1EvMongoDB/js/app.js
@@ -1,9 +1,15 @@
-const {
-    dialog
-} = require('@electron/remote');
-let fs = require("fs");
-
+const {dialog} = require('@electron/remote');
 const mongoose = require('mongoose');
+let mostvotedinfo = document.getElementById("mostvotedinfo");
+let info = document.getElementById("info");
+let vote = document.getElementById("vote");
+let btnvote = document.getElementById("btnvote");
+let btnbefore = document.getElementById("btnbefore");
+let btnnext = document.getElementById("btnnext");
+let img = document.getElementById("img");
+let superheroes;
+let pos = 0;
+let votes;
 
 //promesa por defecto
 mongoose.Promise = global.Promise;
@@ -23,51 +29,47 @@ let sh_esquema = new mongoose.Schema({
     img: {type:String},
     votes: {type:Number}
 });
+
+let votes_esquema = new mongoose.Schema({
+    id: {type:Number},
+    voter: {type:String},
+});
+
 //modelo
 let sh = mongoose.model('superheroes', sh_esquema);
-//buscar datos
+let vt = mongoose.model('votes', votes_esquema);
 
+//buscar datos
 sh.find().then(resultado => {
-    console.log(resultado);
+    superheroes = resultado;
+    console.log(superheroes);
+    loadSuperhero()
+}).catch(error => {
+    console.log("ERROR:", error);
+});
+vt.find().then(resultado => {
+    votes = resultado;
+    console.log(votes);
 }).catch(error => {
     console.log("ERROR:", error);
 });
 
-let superheroes = new Array();
-superheroes = JSON.parse(fs.readFileSync('db/superheroes.json'));
-let votes = new Array();
-votes = JSON.parse(fs.readFileSync('db/votes.json'));
-
-let mostvotedinfo = document.getElementById("mostvotedinfo");
-let info = document.getElementById("info");
-let vote = document.getElementById("vote");
-let btnvote = document.getElementById("btnvote");
-let btnbefore = document.getElementById("btnbefore");
-let btnnext = document.getElementById("btnnext");
-let img = document.getElementById("img");
-
-let pos = 0;
-
-loadSuperhero()
 btnvote.addEventListener('click', ()=>{voteSuperhero()});
 btnbefore.addEventListener('click', ()=>{beforeImg()});
 btnnext.addEventListener('click', ()=>{nextImg()});
 
 function voteSuperhero() {
     let name = vote.value
-    votes.push({
-        "id": pos,
-        "voter": "" + name + ""
+    let nVote = new vt({
+        id: pos,
+        voter: name
     });
-    try {
-        let data = JSON.stringify(votes, null, 2);
-        fs.writeFileSync('db/votes.json', data, (err) => {
-            if (err) throw err;
-            console.log('Data written to file');
-        });
-    } catch (error) {
-        console.log(error);
-    }
+    nVote.save(nVote).then(resultado => {
+        console.log("BIEN:", resultado);
+    }).catch(error => {
+        console.log("ERROR:", error);
+    });
+
     dialog.showMessageBox({
         message: 'Has votado a '+superheroes[pos].superhero+'!',
         type: 'info',
@@ -76,15 +78,7 @@ function voteSuperhero() {
     });
     vote.value="";
     checkMostVoted()
-    try {
-        let datasuperheroes = JSON.stringify(superheroes, null, 2);
-        fs.writeFileSync('db/superheroes.json', datasuperheroes, (err) => {
-            if (err) throw err;
-            console.log('Data written to file');
-        }); 
-    } catch (error) {
-        console.log(error);
-    }
+    
 }
 function checkMostVoted() {
     let idmostvoted;
