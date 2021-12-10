81ac6b56d1684d3b5e8ad7134772d39eed751219
Juan Sáez García <contacto@juamber.com>
2021-12-10 09:39:04 +0100

    quiz

--- 020.Quiz/js/app.js
+++ 020.Quiz/js/app.js
@@ -1,16 +1,54 @@
-let nPreg = 4;
+let fs = require("fs");
+let file = fs.readFileSync('db/test.json');
+let test = new Array();
+test = JSON.parse(file);
+
+cargarTest(test);
+
 document.getElementById("comprobar").addEventListener('click', () => {
+    corregirTest(test);
+});
+
+function corregirTest(test){
     let aciertos = 0;
     let fallos = 0;
-    for (let i = 0; i < nPreg; i++) {
-        let ra = document.getElementById('rA' + (i + 1));
-        let rb = document.getElementById('rB' + (i + 1));
-        let rc = document.getElementById('rC' + (i + 1));
-        //supongo que las respuestas A son siempre las correctas:
-        if (ra.checked) aciertos++;
-        if (rb.checked) fallos++;
-        if (rc.checked) fallos++;
+
+    for (let i = 0; i < test.length; i++) {
+
+        let check1 = document.getElementById("rA"+i).checked
+        let check2 = document.getElementById("rA"+i).checked
+        let check3 = document.getElementById("rA"+i).checked
+
+        switch (test[i].correcta) {
+            case "a": if (check1){aciertos++;}else{fallos++} continue;
+            case "b": if (check2) {aciertos++;}else{fallos++} continue;
+            case "c": if (check3) {aciertos++;}else{fallos++} continue;
+        }
     }
-    document.getElementById("cabecera").innerHTML = "Aciertos: " +
-        aciertos + "   Fallos: " + fallos;
-});
+
+    document.getElementById("cabecera").innerHTML = "Aciertos: " + aciertos + " Fallos:" + fallos
+}
+
+function cargarTest(test) {
+    let preguntas = "";
+    for (let i = 0; i < test.length; i++) {
+
+        preguntas += `
+        <li class="list-group-item"> 
+            <img class="img-circle media-object pull-left" src ="../img/${i+1}.png" width="32" height="32">
+                <div class="media-body">
+                    <strong>${test[i].pregunta}</strong> 
+                    <div class = "radio">
+                        <label><input type="radio" name="radios${i}" id="rA${i}">${test[i].rA}</label> 
+                    </div> 
+                    <div class="radio">
+                        <label><input type="radio" name="radios${i}" id="rB${i}">${test[i].rB}</label>
+                    </div> 
+                    <div class="radio">
+                        <label><input type="radio" name="radios${i}" id="rC${i}">${test[i].rC}</label> 
+                    </div> 
+                </div> 
+        </li>`
+    }
+    document.getElementById("lista-preguntas").innerHTML = preguntas;
+}
