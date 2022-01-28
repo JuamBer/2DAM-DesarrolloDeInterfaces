e40e00808a664e30a0d8b90f19dfc313b2ba4f15
Juan Sáez García <contacto@juamber.com>
2022-01-28 13:45:07 +0100

    .

--- 029.AccesoMongoConInterfaz/js/app.js
+++ 029.AccesoMongoConInterfaz/js/app.js
@@ -1,4 +1,8 @@
+'use strict';
 const mongoose = require('mongoose');
+const search = document.getElementById("search");
+const newBook = document.getElementById("newBook");
+const all = document.getElementById("all");
 
 mongoose.Promise = global.Promise;
 
@@ -7,47 +11,88 @@ mongoose.connect('mongodb://localhost:27017/libros', {
     useUnifiedTopology: true
 });
 
-let libroSchema = new mongoose.Schema({
+let librosSchema = new mongoose.Schema({
     title: {
         type: String,
-        required: true
+        required: true,
+        minlength: 1,
+        trim: true
     },
     author: {
         type: String,
-        required: true
+        required: true,
+        minlength: 1,
+        trim: true
     },
     img: {
-        type: String
+        type: String,
+        required: true,
+        minlength: 1,
+        unique: true,
+        trim: true
     }
 });
 
-let Book = mongoose.model('libros', libroSchema);
+let Libro = mongoose.model('libros', librosSchema);
+//home(Libro);
+search.addEventListener('click', () => {
+    searchBook();
+});
+all.addEventListener('click', () => {
+    home(Libro);
+});
+newBook.addEventListener('click', () => {
+    addNewBook(Libro);
+});
+function searchBook() {
+    
+}
+function addNewBook() {
+    const insertBook_title = document.getElementById("insertBook_title");
+    const insertBook_author = document.getElementById("insertBook_author");
+    const insertBook_img = document.getElementById("insertBook_img");
+    const insertBook_button = document.getElementById("insertBook_button");
 
-printBooks();
+    insertBook_button.addEventListener('click', ()=>{
+        const title = insertBook_title.value;
+        const author = insertBook_author.value;
+        const img = insertBook_img.value;
 
-function printBooks() {
+        const newBook = new Book({
+            title: title,
+            author: author,
+            img: img
+        });
 
-    let books = Book.find().then(resultado => {
-        console.log("FIND: \n\t", resultado);
+        let promise = newBook.save().then(resultado => {
+            console.log("SAVE: \n\t", resultado);
+        }).catch(error => {
+            console.log("SAVE: \n\tERROR:", error);
+        });
+    });
+}
+function home(Libro) {
+    let p1 = Libro.find().then(resultado => {
+        printBooks(resultado);
     }).catch(error => {
-        console.log("FIND: \n\tERROR:", error);
+        console.log("ERROR en find");
     });
-    books.forEach(item => {
-
-    });
-    const wrapper = document.getElementById("wrapper");
-    let books = "";
-
-    books.forEach(book => {
-        books += `
-        <div class="book">
-            <img src="${recurso}/public/${book.img}">
-            <h3>${book.title}</h3>
-            <p>${book.author}</p>
-        </div>`;
-    });
-
-    wrapper.innerHTML = books;
 }
 
 
+function printBooks(resultado) {
+    const wrapper = document.getElementById("wrapper");
+    let booksHTML = "";
+
+    for (let index = 0; index < resultado.length; index++) {
+        const book = resultado[index];
+        booksHTML += `
+            <div class="book">
+                <img src="../img/${book.img}">
+                <h3>${book.title}</h3>
+                <p>${book.author}</p>
+            </div>`;
+    }
+
+    wrapper.innerHTML = booksHTML;
+}
